d424db5f8c6c8dc715b04ca9fa5a42228a226135
asunaharu <152788183+asunaharu@users.noreply.github.com>
2023-12-03 22:49:39 +0800

    User id changed name

--- FinLabAct3_FuentesAngeles/LoginForm.Designer.cs
+++ FinLabAct3_FuentesAngeles/LoginForm.Designer.cs
@@ -84,9 +84,9 @@
             this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.label1.Location = new System.Drawing.Point(66, 156);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 7;
-            this.label1.Text = "ID Number";
+            this.label1.Text = "User ID";
             // 
             // label2
             // 
